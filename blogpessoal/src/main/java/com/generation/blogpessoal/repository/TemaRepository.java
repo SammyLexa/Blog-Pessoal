3274bd3e88dfb4d000740ce5b9557c48584e4c48
Samantha Alexandra <lexalaska31@gmail.com>
2023-03-20 19:57:11 -0300

    Arrumando tema

--- blogpessoal/src/main/java/com/generation/blogpessoal/repository/TemaRepository.java
+++ blogpessoal/src/main/java/com/generation/blogpessoal/repository/TemaRepository.java
@@ -8,6 +8,8 @@ import org.springframework.stereotype.Repository;
 
 import com.generation.blogpessoal.model.Tema;
 
+
+@Repository
 public interface TemaRepository extends JpaRepository<Tema, Long> {
 	
 	public List<Tema> findAllByDescricaoContainingIgnoreCase(@Param("descricao") String descricao);
